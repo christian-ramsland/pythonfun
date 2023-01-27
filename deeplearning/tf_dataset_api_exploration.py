203d31d4ae88abbc5997ba5be3591fd55b073ae5
christian-ramsland <weeaboochamploo@protonmail.com>
2023-01-26 22:15:32 -0500

    a great way to crash your computer

--- deeplearning/tf_dataset_api_exploration.py
+++ deeplearning/tf_dataset_api_exploration.py
@@ -1,5 +1,3 @@
-import glob
-
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -11,6 +9,7 @@ import matplotlib.pyplot as plt # workaround for image viewing in pycharm
 import glob
 import tensorflow_datasets as tfds
 import keras.preprocessing.image
+from tensorflow.python.data import AUTOTUNE
 
 """
 more like a tutorial on kaggle and loading/preprocessing image data
@@ -51,7 +50,6 @@ def preprocess(x,y):
   img = tf.io.read_file(x)
   img = tf.io.decode_jpeg(img, channels=3)
   img = tf.cast(img,dtype=tf.float32)
-  img = img / 255.
   return img, y
 
 
@@ -61,19 +59,62 @@ def main():
     data_dir_train = pathlib.Path(archive_train).with_suffix('')
     image_count_cat = len(list(data_dir_train.glob('cat*.jpg')))
     image_count_dog = len(list(data_dir_train.glob('dog*.jpg')))
+    DATASET_SIZE = image_count_cat + image_count_dog
     print(list(data_dir_train.glob('cat*.jpg')))
-    label_dict = {'cat':0, 'dog':1}
+    label_dict = {'cat': 0, 'dog': 1}
     paths = glob.glob(trainpath + '/*.jpg')
 
     print(get_label(paths[0], label_dict))
-    labels = [get_label(i,label_dict) for i in paths]
+    labels = [get_label(i, label_dict) for i in paths]
     print(labels)
 
-    dataset = tf.data.Dataset.from_tensor_slices((paths,labels))
-    dataset = dataset.map(preprocess)
+    training_dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
+    training_dataset = training_dataset.shuffle(buffer_size=len(paths)) # amazing resource about exactly this: https://stackoverflow.com/questions/46444018/meaning-of-buffer-size-in-dataset-map-dataset-prefetch-and-dataset-shuffle
+    training_dataset = training_dataset.map(preprocess)
 
-    for x, y in dataset.take(1):
-        print(x.shape, y)
+    for x, y in training_dataset.take(1):
+        print("x shape=", x.shape, "y shape=", y.shape)
+
+    train_size = int(0.8 * DATASET_SIZE)
+    val_size = int(0.2 * DATASET_SIZE)
+
+    # tf take and shuffle: https://stackoverflow.com/questions/48213766/split-a-dataset-created-by-tensorflow-dataset-api-in-to-train-and-test
+
+    train_dataset = training_dataset.take(train_size)
+    val_dataset = training_dataset.skip(train_size)
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+    num_classes = 2
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Rescaling(1. / 255),
+
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(num_classes)
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=3
+    )
 
     # that's enough for now, hoping to match with https://www.tensorflow.org/tutorials/load_data/images at some point.
     """
